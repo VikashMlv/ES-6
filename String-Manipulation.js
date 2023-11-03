38484b8e0007d593ccc5c2a19ffc988e4098d496
Vinod Prajapati <vinodprajapatimj@gmail.com>
2023-11-03 05:42:02 -0700

    string manipulation

---
+++ String-Manipulation.js
@@ -0,0 +1,27 @@
+//length property in String
+
+var name  = "rajesh";
+console.log(name.length);
+
+// indexOf property in String
+var index = name.indexOf('ra');
+console.log(index);
+
+//Include 
+
+var data = 'online games';
+console.log(data.includes('online'));  // Result true 
+
+//Slice method in String
+var playerName = 'sachin';
+console.log(playerName.slice(1,3));
+
+//toUppercase method
+var city = "bhopal";
+console.log(city.slice(0,1).toUpperCase());
+
+
+//toLowrcase method in String
+
+var country_name = "india";
+console.log(country_name.slice(0,1).toUpperCase());
